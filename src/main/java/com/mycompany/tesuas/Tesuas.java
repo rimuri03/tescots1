f82e3b586abc75b4b8f36739f205681cc0869c47
Rizki Muhammad Ridwan <127035153+rimuri03@users.noreply.github.com>
2024-05-31 16:11:15 +0700

    Update Tesuas.java

--- src/main/java/com/mycompany/tesuas/Tesuas.java
+++ src/main/java/com/mycompany/tesuas/Tesuas.java
@@ -20,69 +20,19 @@ public class Tesuas {
         System.out.print("Masukkan nilai b: ");
         int b = scanner.nextInt();
         
-        int hasil1 = CariNilaiPangkat1(a, b);
-        if (hasil1 == -1) {
+        int hasil = CariNilaiPangkat(a, b);
+        if (hasil == -1) {
             System.out.println("Nilai pangkat negatif tidak diperbolehkan.");
-        } else if (hasil1 == -2) {
+        } else if (hasil == -2) {
             System.out.println("Input melebihi batas yang diizinkan.");
-        } else {
-            System.out.println("Hasil pangkat dari " + a + "^" + b + " adalah: " + hasil1);
-        }
-
-        int hasil2 = CariNilaiPangkat2(a, b);
-        if (hasil2 == -1) {
-            System.out.println("Nilai pangkat negatif tidak diperbolehkan.");
-        } else if (hasil2 == -2) {
-            System.out.println("Input melebihi batas yang diizinkan.");
-        } else {
-            System.out.println("Hasil pangkat dari " + a + "^" + b + " adalah: " + hasil2);
-        }
-
-        int hasil3 = CariNilaiPangkat3(a, b);
-        if (hasil3 == -1) {
-            System.out.println("Nilai pangkat negatif tidak diperbolehkan.");
-        } else if (hasil3 == -2) {
-            System.out.println("Input melebihi batas yang diizinkan.");
-        } else {
-            System.out.println("Hasil pangkat dari " + a + "^" + b + " adalah: " + hasil3);
+        }else {
+            System.out.println("Hasil pangkat dari " + a + "^" + b + " adalah: " + hasil);
         }
         
         scanner.close();
     }
 
-    public static int CariNilaiPangkat1(int a, int b) {
-        if (b == 0) {
-            return 1;
-        } else if (b < 0) {
-            return -1;
-        } else if (b > 10 || a > 100) {
-            return -2;
-        } else {
-            int result = 1;
-            for (int i = 0; i < b; i++) {
-                result *= a;
-            }
-            return result;
-        }
-    }
-
-    public static int CariNilaiPangkat2(int a, int b) {
-        if (b == 0) {
-            return 1;
-        } else if (b < 0) {
-            return -1;
-        } else if (b > 10 || a > 100) {
-            return -2;
-        } else {
-            int result = 1;
-            for (int i = 0; i < b; i++) {
-                result *= a;
-            }
-            return result;
-        }
-    }
-
-    public static int CariNilaiPangkat3(int a, int b) {
+     public static int CariNilaiPangkat(int a, int b) {
         if (b == 0) {
             return 1;
         } else if (b < 0) {
